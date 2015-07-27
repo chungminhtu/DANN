99b5bef23da474470b470d162ced80909efaa5ff
chungminhtu <chungminhtu@gmail.com>
2015-07-28 01:13:30 +0700

    Load Menu OK from statis BuildMenu

--- DANN.Web/Controllers/ADMenuController.cs
+++ DANN.Web/Controllers/ADMenuController.cs
@@ -10,89 +10,20 @@ using DANN.Service;
 namespace DANN.Web.Controllers
 {
 
-    //public static class MenuCommon
-    //{
-    //    public static DANNContext db = new DANNContext();
-    //    public static IEnumerable<AD_Menu> GetList(int? parentID = null)
-    //    {
-    //        foreach (var item in db.AD_Menu
-    //            .Where(x => x.ParentId == parentID)
-    //           .OrderBy(x => x.MenuSort)
-    //           .ToList())
-    //        {
-    //            yield return item;
-
-    //            foreach (var child in GetList(item.Id))
-    //            {
-    //                yield return child;
-    //            }
-    //        }
-    //    }
-
-    //    public static ASPxMenu BuildMenu(ASPxMenu menu)
-    //    {
-    //        List<AD_Menu> menus = GetList().ToList();
-
-    //        for (int i = 0; i < menus.Count; i++)
-    //        {
-    //            AD_Menu row = menus[i];
-
-    //            MenuItem item = new MenuItem();
-    //            item.Name = row.Id + "";
-    //            item.Text = row.MenuText;
-    //            item.NavigateUrl = DevExpressHelper.GetUrl(new { Controller = row.MenuAction, Action = "Index" }); ;
-    //            item.Image.Url = row.MenuIcon;
-    //            item.BeginGroup = row.MenuSeparator.HasValue ? row.MenuSeparator.Value : false;
-
-    //            if (i == 0 || row.ParentId + "" == "")
-    //            {
-    //                menu.Items.Add(item);
-    //            }
-    //            else
-    //            {
-    //                GetNodes(menu.Items, row.ParentId + "", item);
-    //            }
-    //        }
-
-    //        return menu;
-    //    }
-    //    public static void GetNodes(MenuItemCollection menus, string parentID, MenuItem item)
-    //    {
-    //        if (menus == null)
-    //        {
-    //            return;
-    //        }
-    //        foreach (MenuItem myitem in menus)
-    //        {
-    //            if (myitem.Name == parentID)
-    //            {
-    //                myitem.Items.Add(item);
-    //            }
-    //            GetNodes(myitem.Items, parentID, item);
-    //        }
-    //    }
-
-    //} 
-
-
     public static class MenuCommon
     {
-        public static IAD_MenuService _menuService;
-
-        public static IEnumerable<AD_Menu> GetListOrdered(int? parentID = null)
+        public static DANNContext db = new DANNContext();
+        public static IEnumerable<AD_Menu> GetList(int? parentID = null)
         {
-            int i = 0;
-            foreach (var item in _menuService.GetAllAsQueryable()
+            foreach (var item in db.AD_Menu
                 .Where(x => x.ParentId == parentID)
                .OrderBy(x => x.MenuSort)
                .ToList())
             {
-                item.MenuSort = i++;
                 yield return item;
-                int j = 0;
-                foreach (var child in GetListOrdered(item.Id))
+
+                foreach (var child in GetList(item.Id))
                 {
-                    child.MenuSort = j++;
                     yield return child;
                 }
             }
@@ -100,7 +31,7 @@ namespace DANN.Web.Controllers
 
         public static ASPxMenu BuildMenu(ASPxMenu menu)
         {
-            List<AD_Menu> menus = _menuService.GetList().ToList();
+            List<AD_Menu> menus = GetList().ToList();
 
             for (int i = 0; i < menus.Count; i++)
             {
@@ -122,6 +53,7 @@ namespace DANN.Web.Controllers
                     GetNodes(menu.Items, row.ParentId + "", item);
                 }
             }
+
             return menu;
         }
         public static void GetNodes(MenuItemCollection menus, string parentID, MenuItem item)
@@ -151,7 +83,6 @@ namespace DANN.Web.Controllers
         {
             _service = service;
             _menuService = menuService;
-            MenuCommon._menuService = menuService;
         }
     }
 
