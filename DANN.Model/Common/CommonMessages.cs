0f776a5a924241bad773896c744589b3c330103d
Dong Mai <dongmaicnttk53@gmail.com>
2015-08-03 11:13:30 +0700

    Mai sửa Model bắt lỗi định dạng E-mail

--- DANN.Model/Common/CommonMessages.cs
+++ DANN.Model/Common/CommonMessages.cs
@@ -11,6 +11,7 @@ namespace DANN.Service
         public const string Required = "Bạn chưa nhập \"{0}\"";
         public const string StringLenght = "\"{0}\" không được vượt quá {1} ký tự";
         public const string Range = "{0} chỉ được nhập giá trị là số từ {1} đến {2}.";
+        public const string RegularExpression = "Bạn chưa nhập đúng định dạng E-mail. Ví dụ: joe.example@example.org";
 
 
     }
--- DANN.Model/Models/AD_User.cs
+++ DANN.Model/Models/AD_User.cs
@@ -14,38 +14,40 @@ namespace DANN.Model
             [Required(ErrorMessage = DANN.Service.CommonMessages.Required)]
             [StringLength(30, ErrorMessage = DANN.Service.CommonMessages.StringLenght)]
             public string User_Id { get; set; }
-           
+
             [DisplayName("Mật khẩu")]
             [Required(ErrorMessage = DANN.Service.CommonMessages.Required)]
             [StringLength(20, ErrorMessage = DANN.Service.CommonMessages.StringLenght)]
             public string Password { get; set; }
-           
+
             [DisplayName("Tên người dùng")]
             [Required(ErrorMessage = DANN.Service.CommonMessages.Required)]
             [StringLength(50, ErrorMessage = DANN.Service.CommonMessages.StringLenght)]
             public string UserName { get; set; }
 
             [DisplayName("Địa chỉ Email")]
-            [Required(ErrorMessage=DANN.Service.CommonMessages.Required)]
+            [Required(ErrorMessage = DANN.Service.CommonMessages.Required)]
+            [RegularExpression(@"^([0-9a-zA-Z]([\+\-_\.][0-9a-zA-Z]+)*)+@(([0-9a-zA-Z][-\w]*[0-9a-zA-Z]*\.)+[a-zA-Z0-9]{2,3})$",
+            ErrorMessage = DANN.Service.CommonMessages.RegularExpression)]
             public string Email { get; set; }
 
             [DisplayName("Địa chỉ hiện tại")]
-            [Required(ErrorMessage=DANN.Service.CommonMessages.Required)]
+            [Required(ErrorMessage = DANN.Service.CommonMessages.Required)]
             [StringLength(250, ErrorMessage = DANN.Service.CommonMessages.StringLenght)]
             public string DiaChi { get; set; }
 
             [DisplayName("Số điện thoại")]
-            [Required(ErrorMessage=DANN.Service.CommonMessages.Required)]
-            [Range(0, 50, ErrorMessage = DANN.Service.CommonMessages.Range)]
+            [Required(ErrorMessage = DANN.Service.CommonMessages.Required)]
+            [StringLength(50, ErrorMessage = DANN.Service.CommonMessages.StringLenght)]
             public string SoDienThoai { get; set; }
 
             [DisplayName("Chức vụ")]
-           // [Required("Bạn chưa nhập Chức vụ!")]
+            // [Required("Bạn chưa nhập Chức vụ!")]
             [StringLength(100, ErrorMessage = DANN.Service.CommonMessages.StringLenght)]
             public string ChucVu { get; set; }
-         
+
             [DisplayName("Thuộc Phòng ban")]
-           // [Required("Bạn chưa nhập Phòng ban!")]
+            // [Required("Bạn chưa nhập Phòng ban!")]
             [StringLength(100, ErrorMessage = DANN.Service.CommonMessages.StringLenght)]
             public string PhongBan { get; set; }
         }
