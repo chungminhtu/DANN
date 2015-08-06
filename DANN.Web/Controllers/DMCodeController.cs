85b4d1a608d48c879da5eb1087c2f083a315af9c
Dong Mai <dongmaicnttk53@gmail.com>
2015-08-06 11:41:29 +0700

    Mai sửa phần Delete

--- DANN.Web/Controllers/DMCodeController.cs
+++ DANN.Web/Controllers/DMCodeController.cs
@@ -11,10 +11,12 @@ namespace DANN.Web.Controllers
     public class DMCodeController : CommonController<DM_CodeKind, DM_Code, DM_Code>
     {
         IEntityService<DM_Code> _service { get; set; }
+        IEntityService<DM_CodeKind> _serviceCodeKind { get; set; }
 
-        public DMCodeController(IEntityService<DM_CodeKind> service, IEntityService<DM_Code> service1, IEntityService<DM_Code> service2)
+        public DMCodeController(IEntityService<DM_CodeKind> service, IEntityService<DM_Code> service1, IEntityService<DM_Code> service2 )
             : base(service, service1, service2)
         {
+            _serviceCodeKind = service;
             _service = service1;
         }
 
@@ -23,6 +25,23 @@ namespace DANN.Web.Controllers
         {
             ViewData["ComboListChucVu"] = _service.SearchToList("CodeKind_Id = 2");
             return PartialView("_ComboboxChucVu", model);
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult DeleteCode(DM_CodeKind item)
+        {
+           int dem = _service.SearchToList("CodeKind_Id = "+item.CodeKind_Id).Count();
+            if (dem!=0) 
+           {
+               ViewData["Success"] = "NotDeleteOK1";
+           }
+            else 
+            {
+                _serviceCodeKind.Delete(item);
+                ViewData["Success"] = "DeleteOK";
+            }
+            var model = _serviceCodeKind.GetAll();
+            return PartialView("CodeKind", model);
         }
     }
 }
