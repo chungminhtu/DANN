c3da3c88dfdf5df2a93b456c9f94d821560ba132
tontruong <tonvantruongbk@gmail.com>
2015-08-06 15:50:21 +0700

    update bieudo

---
+++ DANN.Model/Models/View_ThongKeNienGiam.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+namespace DANN.Model
+{
+
+    public partial class View_ThongKeNienGiam : BaseEntity
+    {
+       
+    }
+}
---
+++ DANN.Service/Services/View_ThongKeNienGiamService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DANN.Model;
+
+namespace DANN.Service
+{
+
+    public class View_ThongKeNienGiamService : EntityService<View_ThongKeNienGiam>
+    {
+        public View_ThongKeNienGiamService(IContext context)
+            : base(context)
+        {
+            _context = context;
+            _dbset = _context.Set<View_ThongKeNienGiam>();
+        }
+    }
+
+
+}
